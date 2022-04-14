0f96c83d5090ca5df43f71e2188b39772e5ca191
Haley Parker <90306479@ep-student.org>
2022-04-14 11:19:30 -0500

    how to play popup

---
+++ MessageApp MessagesExtension/popUpViewController.swift
@@ -0,0 +1,20 @@
+//
+//  popUpViewController.swift
+//  MessageApp MessagesExtension
+//
+//  Created by 90306479 on 4/14/22.
+//
+
+import SwiftUI
+
+struct popUpViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct popUpViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        popUpViewController()
+    }
+}
